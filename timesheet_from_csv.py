6639e43d2e04e1a2904735869fe83ac449d67f76
xhamyd <xhamyd@users.noreply.github.com>
2015-07-11 00:36:10 -0400

    Update timesheet_from_csv.py

--- timesheet_from_csv.py
+++ timesheet_from_csv.py
@@ -1,7 +1,26 @@
+"""
+    A timesheet is represented by a 2D list, with 2 1D list for the headers
+"""
+
 import csv
 
-def run():
-    print "This is not complete"
-    return
-    
-run()
+class Timesheet(object):
+    def __init__(self, L, dayHeader, timeHeader):
+        self.L = L
+        self.dayHeader = dayHeader
+        self.timeHeader = timeHeader
+
+    def getIndices(self, day, time):
+        return (self.dayHeader.index(day), self.timeHeader.index(time))
+        
+    def checkAvail(self, day, time):
+        (dayIndex, timeIndex) = getIndices(self, day, time)
+        return self.L[dayIndex][timeIndex] == "x"
+
+    def markBusy(self, day, time):
+        (dayIndex, timeIndex) = getIndices(self, day, time)
+        return self.L[dayIndex][timeIndex] = "x"
+
+    def markFree(self, day, time):
+        (dayIndex, timeIndex) = getIndices(self, day, time)
+        return self.L[dayIndex][timeIndex] = ""
