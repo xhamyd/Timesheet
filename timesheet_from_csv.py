097aeb0a3a88d8891c78db37cce7d37877f3d884
xhamyd <xhamyd@users.noreply.github.com>
2015-07-11 09:19:00 -0400

    Update timesheet_from_csv.py

--- timesheet_from_csv.py
+++ timesheet_from_csv.py
@@ -2,6 +2,7 @@
     A timesheet is represented by a 2D list, with 2 1D list for the headers
 """
 
+import string
 import csv
 
 class Timesheet(object):
@@ -9,6 +10,7 @@ class Timesheet(object):
         self.L = L
         self.dayHeader = dayHeader
         self.timeHeader = timeHeader
+        self.timeIncrement = self.calcTimeIncr(timeHeader)
 
     def getIndices(self, day, time):
         return (self.dayHeader.index(day), self.timeHeader.index(time))
@@ -25,6 +27,31 @@ class Timesheet(object):
         (dayIndex, timeIndex) = getIndices(self, day, time)
         return self.L[dayIndex][timeIndex] = ""
         
+    def calcTimeIncr(self, timeHeader):
+        if len(timeHeader) > 1: return subTime(timeHeader[1], timeHeader[0])
+        else: return False #there is no timeIncrement
+
+class Time(object):
+    def __init__(self, hours, mins, period):
+        self.hours = hours
+        self.mins = mins
+        self.period = period.upper()
+        
+    def __repr__(self):
+        return "%02d:%02d %s" % (self.hours, self.mins, self.period)
+        
+    def __sub__(self, other):
+        hours1 = self.hours + 12 if (self.period == "PM") else self.hours
+        hours1 = 0 if (self.period == "AM" and self.hours == 12) else hours1
+        
+        hours2 = other.hours + 12 if (other.period == "PM") else other.hours
+        hours2 = 0 if (other.period == "AM" and other.hours == 12) else hours2
+        
+        
+        
+class Conflict(object):
+    def __init__(self, timesheet, startTime, endTime)
+    
 def timesheet_from_csv():
     timesheet_file = open(TIMESHEET_FROM_CSV_FILE), 'rU')
     timeHeader, L = [], [[]]
