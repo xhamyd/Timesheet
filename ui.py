d8ac71f256c1f892cce2206fcc9a114d4d32707d
xhamyd <xhamyd@users.noreply.github.com>
2015-07-12 11:42:55 -0400

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,7 @@
+def incorrectTypes(T, day, startTime, endTime):
+
+def add(T, day, startTime, endTime):
+    if incorrectTypes(T, day, startTime, endTime):
+        #bad types, do not execute function
+        
+    
