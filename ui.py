07ed82ca9b34a34d28725a72a8ffd40c0fbc4183
xhamyd <xhamyd@users.noreply.github.com>
2015-07-13 20:00:00 -0400

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,11 +1,20 @@
+import timesheet_from_csv as TFC
+
 def incorrectTypes(T, day, startTime, endTime):
     return (type(T) != Timesheet or
             type(day) != string or
             type(startTime) != Time or
             type(endTime) != Time)
 
-def add(T, day, startTime, endTime):
+def calcNumSlots(T, startTime, endTime):
+    duration = endTime - startTime
+    return duration / T.timeIncrement
+    
+def add(T, day, startTime, endTime): #add a time when busy (conflict)
     if incorrectTypes(T, day, startTime, endTime):
         #bad types, do not execute function
         
+    numSlots = calcNumSlots(T.startTime, startTime, endTime)
+    for i in xrange(numSlots):
+        T.markBusy(day
     
