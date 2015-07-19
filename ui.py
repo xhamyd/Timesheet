88e48f2eb11035254ef4e2da739cc64c6ca109fb
xhamyd <xhamyd@users.noreply.github.com>
2015-07-19 13:14:14 -0400

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-import timesheet_from_csv as TFC
+from timesheet_from_csv import *
 import math
 
 def incorrectTypes(T, day, startTime, endTime):
@@ -7,16 +7,27 @@ def incorrectTypes(T, day, startTime, endTime):
             type(startTime) != Time or
             type(endTime) != Time)
 
+def missingFromHeaders(T, day, startTime, endTime):
+    #balls
+    
+def REQUIRES_CHECK(T, day, startTime, endTime):
+    return (not incorrectTypes(T, day, startTime, endTime) and
+            not missingFromHeaders(T, day, startTime, endTime))
+            
 def calcNumSlots(T, startTime, endTime):
-    duration = endTime - startTime
-    return math.ceil(duration / T.timeIncrement)
+    sT = maxTime(startTime, T.startingTime)
+    eT = minTime(endTime, T.endingTime)
+    duration = eT - sT
+    return int(math.ceil(duration / T.timeIncrement))
     
 def add(T, day, startTime, endTime): #add a time when busy (conflict)
-    if incorrectTypes(T, day, startTime, endTime):
+    if not REQUIRES_CHECK(T, day, startTime, endTime):
         #bad types, do not execute function
+        print "Could not add timeslot as requested, please check the types of the files passed in"
         
     numSlots = calcNumSlots(T, startTime, endTime)
-    for i in xrange(numSlots): T.markBusy(day, startTime + i * T.timeIncrement)
+    for i in xrange(numSlots): 
+        T.markBusy(day, startTime + i * T.timeIncrement)
     
     
     
