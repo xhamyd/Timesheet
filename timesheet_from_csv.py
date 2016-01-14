10a8a22fdfa7cf9bd2b888bf7e6400617b50660c
xhamyd <xhamyd@users.noreply.github.com>
2016-01-14 11:52:57 -0500

    Update timesheet_from_csv.py
    Typos, added automatic call to the function

--- timesheet_from_csv.py
+++ timesheet_from_csv.py
@@ -7,7 +7,7 @@ import csv
 from Classes import Timesheet
 
 def timesheet_from_csv(TIMESHEET_FROM_CSV_FILE):
-    timesheet_file = open(TIMESHEET_FROM_CSV_FILE), 'rU')
+    timesheet_file = open(TIMESHEET_FROM_CSV_FILE, 'rU')
     timeHeader, L = [""], [[]]
     for i, line in enumerate(timesheet_file):
         columns = line.strip().split(',')
@@ -20,3 +20,6 @@ def timesheet_from_csv(TIMESHEET_FROM_CSV_FILE):
         L[i - 1] = columns
     
     return Timesheet(TIMESHEET_FROM_CSV_FILE.strip(".csv"), L, dayHeader, timeHeader)
+
+T = timesheet_from_csv(Timesheet CSV Format - Template.csv)
+return T
